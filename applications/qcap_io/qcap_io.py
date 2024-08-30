6502ab6ff9e070515c29d5fe37252c68ef394d92
IGX <Developer@smartsurgerytek.com>
2024-08-30 03:52:52 +0000

    Add Q-Cap Application

---
+++ applications/qcap_io/qcap_io.py
@@ -0,0 +1,16 @@
+import os
+from holoscan.core import Application
+from holoscan.qcap_source import QCapSourceOp
+class QcapIO(Application):
+    def __init__(self):
+        super().__init__()
+        self.name = "Qcap IO App"
+
+    def compose(self):
+        pass
+        
+if __name__ == "__main__":
+    config_file = os.path.join(os.path.dirname(__file__), "qcap_io.yaml")
+    app = QcapIO()
+    app.config(config_file)
+    app.run()
